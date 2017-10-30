44eb8664e424759835d4aafe6d66aef2752483fb
ameedzain <32744142+ameedzain@users.noreply.github.com>
2017-10-30 12:59:06 +0530

    Update SignUpDone.java

--- app/src/main/java/com/dotvoid/drona/SignUpDone.java
+++ app/src/main/java/com/dotvoid/drona/SignUpDone.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
