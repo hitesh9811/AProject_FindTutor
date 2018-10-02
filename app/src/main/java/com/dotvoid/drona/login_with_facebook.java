206dc04a927c7100b58d0f9416c65ab80a5b7172
vishalbachchas <vishalbachchas08@gmail.com>
2018-10-02 15:09:54 +0530

    Update login_facebook
    Set is updated

--- app/src/main/java/com/dotvoid/drona/login_with_facebook.java
+++ app/src/main/java/com/dotvoid/drona/login_with_facebook.java
@@ -3,25 +3,25 @@ package com.dotvoid.drona;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
+import android.webkit.WebSet1tings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class login_with_facebook extends AppCompatActivity {
 
     WebView w;
-    WebSettings set;
+    WebSet1tings set1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_with_facebook);
+        set1ContentView(R.layout.activity_login_with_facebook);
 
         w=(WebView)findViewById(R.id.web_view);
-        set=w.getSettings();
-        set.setBuiltInZoomControls(true);
-        w.setWebViewClient(new go_back());
-       // w.loadUrl("file:///android_asset/index.html");
+        set1=w.getSet1tings();
+        set1.set1BuiltInZoomControls(true);
+        w.set1WebViewClient(new go_back());
+       // w.loadUrl("file:///android_asset1/index.html");
         w.loadUrl("https://www.facebook.com");
     }
 
